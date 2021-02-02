988bca71eddf4cbc78274972e3add9f3718e2429
anna baek <yeon376@yonsei.ac.kr>
2021-02-02 10:33:28 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true, // 중간 공백 제거
+        unique: 1 // 중복확인
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: { // User의 종류 차등
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+// 모델은 스키마를 감싸주는 역할
+
+module.exports = { User }
